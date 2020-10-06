be4c96dc1adffba1ef369656b179ca06dead926e
YAMADA Masaki <masaki@metabolics.co.jp>
2020-10-06 15:48:12 +0900

    SideBar にページ構成から名前を入れた

--- packages/proto-knowledge-grapher2/lib/components/programs/ProgramsPage.jsx
+++ packages/proto-knowledge-grapher2/lib/components/programs/ProgramsPage.jsx
@@ -2,6 +2,21 @@ import React, {useRef} from 'react'
 import {registerComponent, useSingle2} from 'meteor/vulcan:core'
 import {SidebarComponent, TreeViewComponent} from '@syncfusion/ej2-react-navigations'
 import {Programs} from '../../modules/programs'
+import {v1 as uuidv1} from 'uuid'
+
+const generateNodeId = () => uuidv1()
+
+const children2array = children => {
+  return children.map(node => {
+    const template = {
+      nodeId: generateNodeId(),
+      nodeText: node.name,
+      iconCss: 'icon-circle-thin icon',
+    }
+    if (node.children) template.nodeChild = children2array(node.children)
+    return template
+  })
+}
 
 const ProgramsPage = ({match}) => {
   const {document} = useSingle2({
@@ -9,64 +24,17 @@ const ProgramsPage = ({match}) => {
     fragmentName: 'ProgramFragment',
     input: {id: match.params._id},
   })
-  console.log({document})
 
-  const data = [
+  const structure = document && document.structure
+
+  const data = structure ? [
     {
-      nodeId: '01', nodeText: 'Installation', iconCss: 'icon-microchip icon',
-      nodeChild: [
-        {nodeId: '05-01', nodeText: 'Calendar', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-02', nodeText: 'DatePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-03', nodeText: 'DateTimePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-04', nodeText: 'DateRangePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-05', nodeText: 'TimePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-06', nodeText: 'SideBar', iconCss: 'icon-circle-thin icon'},
-      ],
+      nodeId: generateNodeId(),
+      nodeText: structure.title,
+      iconCss: 'icon-microchip icon',
+      nodeChild: children2array(structure.children),
     },
-    {
-      nodeId: '02', nodeText: 'Deployment', iconCss: 'icon-thumbs-up-alt icon',
-    },
-    {
-      nodeId: '03', nodeText: 'Quick Start', iconCss: 'icon-docs icon',
-    },
-    {
-      nodeId: '04', nodeText: 'Components', iconCss: 'icon-th icon',
-      nodeChild: [
-        {nodeId: '04-01', nodeText: 'Calendar', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '04-02', nodeText: 'DatePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '04-03', nodeText: 'DateTimePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '04-04', nodeText: 'DateRangePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '04-05', nodeText: 'TimePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '04-06', nodeText: 'SideBar', iconCss: 'icon-circle-thin icon'},
-      ],
-    },
-    {
-      nodeId: '05', nodeText: 'API Reference', iconCss: 'icon-code icon',
-      nodeChild: [
-        {nodeId: '05-01', nodeText: 'Calendar', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-02', nodeText: 'DatePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-03', nodeText: 'DateTimePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-04', nodeText: 'DateRangePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-05', nodeText: 'TimePicker', iconCss: 'icon-circle-thin icon'},
-        {nodeId: '05-06', nodeText: 'SideBar', iconCss: 'icon-circle-thin icon'},
-      ],
-    },
-    {
-      nodeId: '06', nodeText: 'Browser Compatibility', iconCss: 'icon-chrome icon',
-    },
-    {
-      nodeId: '07', nodeText: 'Upgrade Packages', iconCss: 'icon-up-hand icon',
-    },
-    {
-      nodeId: '08', nodeText: 'Release Notes', iconCss: 'icon-bookmark-empty icon',
-    },
-    {
-      nodeId: '09', nodeText: 'FAQ', iconCss: 'icon-help-circled icon',
-    },
-    {
-      nodeId: '10', nodeText: 'License', iconCss: 'icon-doc-text icon',
-    },
-  ]
+  ] : []
 
   const width = '290px'
   const target = '.main-content'
