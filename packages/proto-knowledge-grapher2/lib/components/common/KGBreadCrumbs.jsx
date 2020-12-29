749ec9082c75a87b756ea300219a4201a8fa27a8
YAMADA Masaki <masaki@metabolics.co.jp>
2020-12-29 22:59:51 +0900

    KGBreadCrumbsを実装した
    かなり富豪的であるが，まぁいいだろう

--- packages/proto-knowledge-grapher2/lib/components/common/KGBreadCrumbs.jsx
+++ packages/proto-knowledge-grapher2/lib/components/common/KGBreadCrumbs.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-import {registerComponent} from 'meteor/vulcan:core'
+import {registerComponent, useMulti2} from 'meteor/vulcan:core'
 
-const KGBreadCrumbs = ({programId, sectionId}) => (
-    <div>dummy / bread / crumbs</div>
-)
+const findPath = (me, target, path) => {
+  if (!me.children || me.children.length === 0) return null
+  if (me.name) path.push(me.name)
+  if (me.children.find(it => it.id === target)) return path
+  return me.children.map(it => findPath(it, target, Array.from(path))).find(it => it && it.length > 0)
+}
+
+const KGBreadCrumbs = ({programId, sectionId}) => {
+  const {results, loading} = useMulti2(
+      {collectionName: 'Programs', fragmentName: 'ProgramFragment', input: {filter: {_id: {_eq: programId}}}}
+  )
+  if (loading || !results || results.length === 0) return <div/>
+  const program = results[0]
+
+  const path = findPath(program.structure, sectionId, [])
+
+  const programLink = <a href={`/programs/${programId}`}>{results[0].title}</a>
+  const sections = path ? path.join(' / ') : ""
+
+  return (
+      <React.Fragment>
+      {
+        results[0].title ?
+            <div>{programLink} / {sections}</div> :
+          <div/>
+      }
+      </React.Fragment>
+  )
+}
 
 registerComponent({name: 'KGBreadCrumbs', component: KGBreadCrumbs})
