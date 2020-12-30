03db7d2fe0a21ef88cca2e2747be1597c3bc0772
YAMADA Masaki <masaki@metabolics.co.jp>
2020-12-30 18:56:20 +0900

    とりあえずnode, edgeのアノテーション（ラベル）をProgram定義から取ってこられるようにした
    しかし，SymbolPaletteのバグにより，コネクタのアノテーションがうまく反映されない．これはNodeのカスタム・コンテキスト・メニュによって解決する予定．
    ユーザ名も設定した

--- packages/proto-knowledge-grapher2/lib/components/sections/CFNetworkDiagramSection.jsx
+++ packages/proto-knowledge-grapher2/lib/components/sections/CFNetworkDiagramSection.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useRef, useState} from 'react'
-import {Components, registerComponent, useCreate2, useMulti2, useUpdate2} from 'meteor/vulcan:core'
+import {Components, registerComponent, useCreate2, useMulti2, useUpdate2, withCurrentUser} from 'meteor/vulcan:core'
 import {
   DiagramComponent,
   DiagramContextMenu,
@@ -8,65 +8,19 @@ import {
   PrintAndExport,
   SymbolPaletteComponent,
   UndoRedo,
+  AnnotationConstraints,
 } from '@syncfusion/ej2-react-diagrams'
 import {ItemDirective, ItemsDirective, ToolbarComponent} from '@syncfusion/ej2-react-navigations'
 import {Helmet} from 'react-helmet'
 
-const basicShapes = [
-  {
-    id: 'Rectangle-top',
-    shape: {type: 'Basic', shape: 'Rectangle'},
-    annotations: [{offset: {x: 0.5, y: 0.1}, verticalAlignment: 'Top', content: 'ラベル'}],
-    symbolInfo: {description: {text: 'Top'}},
-  }, {
-    id: 'Rectangle-center',
-    shape: {type: 'Basic', shape: 'Rectangle'},
-    annotations: [{offset: {x: 0.5, y: 0.5}, verticalAlignment: 'Center'}],
-    symbolInfo: {description: {text: 'Center'}},
-  }, {
-    id: 'Rectangle-bottom',
-    shape: {type: 'Basic', shape: 'Rectangle'},
-    annotations: [{offset: {x: 0.5, y: 0.9}, verticalAlignment: 'Bottom'}],
-    symbolInfo: {description: {text: 'Bottom'}},
-  }, {
-    id: 'Ellipse',
-    shape: {type: 'Basic', shape: 'Ellipse'},
-  }, {
-    id: 'Hexagon',
-    shape: {type: 'Basic', shape: 'Hexagon'},
-  }, {
-    id: 'Text',
-    shape: {type: 'Text', content: 'Text'},
-    width: 80, height: 40,
-  },
-]
+const findParams = (children, sectionId) => {
+  if (!children || children.length === 0) return null
+  const found = children.find(it => it.id === sectionId)
+  if (found) return found
+  return children.map(it => findParams(it.children, sectionId)).find(it => it != null)
+}
 
-const points = {sourcePoint: {x: 0, y: 0}, targetPoint: {x: 40, y: 40}}
-const connectors = [
-  {id: 'SingleOrthogonal', type: 'Orthogonal', ...points, targetDecorator: {shape: 'Arrow'}},
-  {id: 'SingleStraight', type: 'Straight', ...points, targetDecorator: {shape: 'Arrow'}},
-  {id: 'SingleBezier', type: 'Bezier', ...points, targetDecorator: {shape: 'Arrow'}},
-  {
-    id: 'BothOrthogonal',
-    type: 'Orthogonal', ...points,
-    sourceDecorator: {shape: 'Arrow'},
-    targetDecorator: {shape: 'Arrow'},
-  },
-  {
-    id: 'BothStraight',
-    type: 'Straight', ...points,
-    sourceDecorator: {shape: 'Arrow'},
-    targetDecorator: {shape: 'Arrow'},
-  },
-  {id: 'BothBezier', type: 'Bezier', ...points, sourceDecorator: {shape: 'Arrow'}, targetDecorator: {shape: 'Arrow'}},
-]
-
-const palettes = [
-  {id: 'basic', expanded: true, symbols: basicShapes, title: 'Basic Shapes', iconCss: 'e-ddb-icons e-basic'},
-  {id: 'connectors', expanded: true, symbols: connectors, title: 'Connectors', iconCss: 'e-ddb-icons e-connector'},
-]
-
-const CFNetworkDiagramSection = ({match}) => {
+const CFNetworkDiagramSection = ({match, currentUser}) => {
   const {params} = match
   const collectionName = params.collectionName || 'CFNetworkDiagrams'
   const {programId, sectionId, subsection} = params
@@ -82,13 +36,58 @@ const CFNetworkDiagramSection = ({match}) => {
   })
   if (results && results.length === 0) results[0] = {programId, sectionId, subsection}
 
-  const {nodeLabels = ['undefined'], edgeLabels = ['undefined']} = params
-
   const [updateDocument, {loading: loading_u}] = useUpdate2({collectionName, fragmentName: 'CFNetworkDiagramFragment'})
   const [createDocument, {loading: loading_c}] = useCreate2({collectionName, fragmentName: 'CFNetworkDiagramFragment'})
   const [error, setError] = useState()
 
   const doc = results && results[0] || {}
+
+  const {results: programs, loading_program} = useMulti2(
+      {collectionName: 'Programs', fragmentName: 'ProgramFragment', input: {filter: {_id: {_eq: programId}}}},
+  )
+  const program = programs && programs[0] && programs[0].structure
+  const {nodeLabels, edgeLabels} = findParams((program || []).children, sectionId) || {}
+
+  const basicShapes = (nodeLabels || []).map(nodeLabel => (
+      {
+        id: `node-template-${nodeLabel}`,
+        shape: {type: 'Basic', shape: 'Rectangle'},
+        annotations: [
+          {
+            offset: {x: 0.5, y: 0}, content: nodeLabel, constraints: AnnotationConstraints.ReadOnly,
+            horizontalAlignment: 'Center', verticalAlignment: 'Bottom', style: {bold: true},
+          },
+          {
+            offset: {x: 0.5, y: 0.5}, content: '発言',
+            horizontalAlignment: 'Center', verticalAlignment: 'Center',
+          },
+          {
+            offset: {x: 0.5, y: 1}, content: currentUser.slug, constraints: AnnotationConstraints.ReadOnly,
+            horizontalAlignment: 'Center', verticalAlignment: 'Top', style: {italic: true},
+          },
+        ],
+        symbolInfo: {description: {text: nodeLabel}},
+      }
+  ))
+
+  const points = {sourcePoint: {x: 0, y: 0}, targetPoint: {x: 40, y: 40}}
+
+  const connectors = (edgeLabels || []).map(edgeLabel => (
+      {
+        id: 'SingleStraight', type: 'Straight', ...points, targetDecorator: {shape: 'Arrow'},
+        annotations: [
+          {
+            content: edgeLabel, constraints: AnnotationConstraints.ReadOnly,
+            horizontalAlignment: 'Center', verticalAlignment: 'Center',
+          },
+        ],
+      }
+  ))
+
+  const palettes = [
+    {id: 'basic', expanded: true, symbols: basicShapes, title: '発言', iconCss: 'e-ddb-icons e-basic'},
+    {id: 'connectors', expanded: true, symbols: connectors, title: '発言間の関係性', iconCss: 'e-ddb-icons e-connector'},
+  ]
 
   const diagram = useRef()
 
@@ -147,37 +146,20 @@ const CFNetworkDiagramSection = ({match}) => {
     }
   }
 
-  const nodesTemplate = nodeLabels.map((label, index) => ({
-    id: `node-${index}`,
-    shape: {type: 'Basic', shape: 'Rectangle'},
-    annotations: [
-        {offset: {x: 0.5, y: 0.1}, verticalAlignment: 'Top', content: label},
-    ],
-    symbolInfo: {description: {text: 'Top'}},
-  }))
-
-  const edgesTemplate = edgeLabels.map((label, index) => ({
-    id: 'SingleStraight', type: 'Straight', ...points, targetDecorator: {shape: 'Arrow'},
-  }))
-
-  const templates = [
-    {id: 'nodes', expanded: true, symbols: nodesTemplate, title: '発言', iconCss: 'e-ddb-icons e-basic'},
-    {id: 'edges', expanded: true, symbols: edgesTemplate, title: '位置付け', iconCss: 'e-ddb-icons e-connector'},
-  ]
-
   return (
       <React.Fragment>
         <Helmet><title>Async Session {sectionId} - {subsection})</title></Helmet>
         {doc.title && <h2>{doc.title}</h2>}
-        <SymbolPaletteComponent id='palette' expandMode='Multiple' symbolHeight={64} symbolWidth={64}
-                                scrollSettings={{horizontalOffset: 80, verticalOffset: 40}} palettes={palettes}/>
+        <SymbolPaletteComponent id='palette' expandMode='Multiple' symbolHeight={80} symbolWidth={80}
+                                scrollSettings={{horizontalOffset: 100, verticalOffset: 50}} palettes={palettes}
+                                getSymbolInfo={symbol => symbol.symbolInfo}/>
         {
           error ? <Components.Flash message={error}/> :
-              [loading_c, loading_u].some(it => it === true) ? <Components.Loading/> :
+              [loading_c, loading_u, loading_program].some(it => it === true) ? <Components.Loading/> :
                   <React.Fragment>
                     <DiagramComponent id='diagram' width='100%' height='1000px' ref={diagram}
                                       contextMenuSettings={{show: true}}
-                                      historyChange={e=>console.log({e})}>
+                                      historyChange={e => console.log({e})}>
                       <Inject services={[UndoRedo, DiagramContextMenu, PrintAndExport]}/>
                     </DiagramComponent>
                     <OverviewComponent id="overview" style={{top: '30px'}} sourceID="diagram" width={'100%'}
@@ -200,4 +182,4 @@ const CFNetworkDiagramSection = ({match}) => {
   )
 }
 
-registerComponent({name: 'CFNetworkDiagramSection', component: CFNetworkDiagramSection})
+registerComponent({name: 'CFNetworkDiagramSection', component: CFNetworkDiagramSection, hocs: [withCurrentUser]})
