9a91d8dca81c65a7b857a10e098500cd26184cdc
YAMADA Masaki <masaki@metabolics.co.jp>
2021-01-11 12:51:01 +0900

    KGSectionHeaderを少し見やすくする

--- packages/proto-knowledge-grapher2/lib/components/common/KGSectionHeader.jsx
+++ packages/proto-knowledge-grapher2/lib/components/common/KGSectionHeader.jsx
@@ -4,6 +4,8 @@ import {registerComponent} from 'meteor/vulcan:core'
 const headerStyle = {
   padding: '1rem 2rem',
   borderLeft: '6px double #000',
+  background: '#f4f4f4',
+  margin: '1rem 0rem'
 }
 
 const KGSectionHeader = ({sectionName}) => (
